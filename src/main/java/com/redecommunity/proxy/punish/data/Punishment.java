2ce05dae0d3d5e5f59d17e66d969f5f50d4f31d8
Gutyerrez <srgutyerrez@gmail.com>
2019-12-14 10:22:42 -0300

    Create Punishment.java

---
+++ src/main/java/com/redecommunity/proxy/punish/data/Punishment.java
@@ -0,0 +1,60 @@
+package com.redecommunity.proxy.punish.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@AllArgsConstructor
+public class Punishment {
+    @Getter
+    private final Integer id,
+            userId,
+            stafferId,
+            motiveId,
+            revokerId,
+            revokeMotiveId;
+
+    private final Boolean hidden,
+            perpetual;
+
+    @Getter
+    @Setter
+    private Boolean status;
+
+    @Getter
+    private final String network,
+            revokeReason,
+            proof;
+
+    @Getter
+    private final Long time,
+            startTime,
+            endTime,
+            revokeTime;
+
+    public Boolean isHidden() {
+        return this.hidden;
+    }
+
+    public Boolean isPerpetual() {
+        return this.perpetual;
+    }
+
+    public Boolean isActive() {
+        if (System.currentTimeMillis() >= this.endTime) {
+            this.status = false;
+            // TODO not implemented yet
+        }
+
+        return this.status;
+    }
+
+    public ChatColor getColor() {
+        if (this.revokerId != null) return ChatColor.GRAY;
+        return this.startTime == null ? ChatColor.GOLD : this.status ? ChatColor.GREEN : ChatColor.RED;
+    }
+}
