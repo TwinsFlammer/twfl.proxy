ddf3b3d53a149231f930b9bf0480e065ce4f200b
Gutyerrez <srgutyerrez@gmail.com>
2019-12-15 13:37:25 -0300

    Create PunishmentManager.java

---
+++ src/main/java/com/redecommunity/proxy/punish/manager/PunishmentManager.java
@@ -0,0 +1,32 @@
+package com.redecommunity.proxy.punish.manager;
+
+import com.redecommunity.proxy.punish.data.Punishment;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PunishmentManager {
+
+    public static Punishment toPunishment(ResultSet resultSet) throws SQLException {
+        return new Punishment(
+                resultSet.getInt("id"),
+                resultSet.getInt("user_id"),
+                resultSet.getInt("staffer_id"),
+                resultSet.getInt("motive_id"),
+                resultSet.getInt("revoker_id"),
+                resultSet.getInt("revoker_motive_id"),
+                resultSet.getBoolean("hidden"),
+                resultSet.getBoolean("perpetual"),
+                resultSet.getBoolean("status"),
+                resultSet.getString("network"),
+                resultSet.getString("proof"),
+                resultSet.getLong("time"),
+                resultSet.getLong("start_time"),
+                resultSet.getLong("end_time"),
+                resultSet.getLong("revoker_time")
+        );
+    }
+}
