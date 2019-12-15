78be902ff06eed655dd1cad284ecdf32ee75b69a
Gutyerrez <srgutyerrez@gmail.com>
2019-12-15 13:02:15 -0300

    Update ProxyServerPingListener.java

---
+++ src/main/java/com/redecommunity/proxy/connection/listeners/ProxyServerPingListener.java
@@ -0,0 +1,18 @@
+package com.redecommunity.proxy.connection.listeners;
+
+import com.redecommunity.proxy.Proxy;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class ProxyServerPingListener implements Listener {
+    @EventHandler
+    public void onPing(ServerPing event) {
+        ServerPing.Players players = event.getPlayers();
+
+        players.setOnline(Proxy.getUsers().size());
+    }
+}
