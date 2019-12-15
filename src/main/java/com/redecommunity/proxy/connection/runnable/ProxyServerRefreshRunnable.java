95d4db8e8f271e5ac88451015bc0c9e98221a0fc
Gutyerrez <srgutyerrez@gmail.com>
2019-12-15 12:39:45 -0300

    Create ProxyServerRefreshRunnable.java

---
+++ src/main/java/com/redecommunity/proxy/connection/runnable/ProxyServerRefreshRunnable.java
@@ -0,0 +1,22 @@
+package com.redecommunity.proxy.connection.runnable;
+
+import com.redecommunity.proxy.connection.dao.ProxyDao;
+import com.redecommunity.proxy.connection.data.ProxyServer;
+import com.redecommunity.proxy.connection.manager.ProxyServerManager;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class ProxyServerRefreshRunnable implements Runnable {
+    @Override
+    public void run() {
+        ProxyDao proxyDao = new ProxyDao();
+
+        proxyDao.findAll().forEach(proxyServer -> {
+            ProxyServer proxyServer1 = ProxyServerManager.getProxyServer(proxyServer.getId());
+
+            if (proxyServer1 != null) proxyServer1.setPlayerCount(proxyServer.getPlayerCount());
+            else ProxyServerManager.addProxyServer(proxyServer);
+        });
+    }
+}
