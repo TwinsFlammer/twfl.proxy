781197dbca3994084643f93b223f93e69ea7007e
Gutyerrez <srgutyerrez@gmail.com>
2019-12-19 08:52:50 -0300

    Update Proxy.java

--- src/main/java/com/redecommunity/proxy/Proxy.java
+++ src/main/java/com/redecommunity/proxy/Proxy.java
@@ -1,10 +1,15 @@
 package com.redecommunity.proxy;
 
-import com.google.common.collect.Lists;
 import com.redecommunity.api.bungeecord.CommunityPlugin;
-import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.configuration.ProxyConfiguration;
+import com.redecommunity.proxy.connection.manager.ProxyServerManager;
 import com.redecommunity.proxy.manager.StartManager;
+import lombok.Getter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collection;
 
 /**
@@ -17,8 +22,20 @@ public class Proxy extends CommunityPlugin {
         Proxy.instance = this;
     }
 
+    private ClassLoader classLoader = this.getClass().getClassLoader();
+
+    @Getter
+    private Integer id;
+    @Getter
+    private String name;
+
     @Override
     public void onEnablePlugin() {
+        ProxyConfiguration proxyConfiguration = new ProxyConfiguration();
+
+        this.id = proxyConfiguration.getId();
+        this.name = proxyConfiguration.getName();
+
         new StartManager();
     }
 
@@ -31,9 +48,28 @@ public class Proxy extends CommunityPlugin {
         return Proxy.instance;
     }
 
-    public static Collection<User> getUsers() {
-        Collection<User> users = Lists.newArrayList();
+    public static Collection<Integer> getUsers() {
+        return ProxyServerManager.getUsers();
+    }
 
-        return users;
+
+    public InputStream getResource(String filename) {
+        if (filename == null) {
+            throw new IllegalArgumentException("Filename cannot be null");
+        } else {
+            try {
+                URL url = this.classLoader.getResource(filename);
+                if (url == null) {
+                    return null;
+                } else {
+                    URLConnection connection = url.openConnection();
+                    connection.setUseCaches(false);
+                    return connection.getInputStream();
+                }
+            } catch (IOException var4) {
+                var4.printStackTrace();
+                return null;
+            }
+        }
     }
 }
