0b4ac61f60777a446bb9b4fb2e29b2f606b74575
Gutyerrez <srgutyerrez@gmail.com>
2019-12-19 07:22:05 -0300

    Update Messages.java

--- src/main/java/com/redecommunity/proxy/util/Messages.java
+++ src/main/java/com/redecommunity/proxy/util/Messages.java
@@ -4,10 +4,25 @@ package com.redecommunity.proxy.util;
  * Created by @SrGutyerrez
  */
 public class Messages {
+    private static final String PREFIX = "§c§lREDE COMMUNITY";
+
     public static final String INVALID_UUID_MESSAGE = "\n" +
+            PREFIX +
+            "\n" +
+            "\n" +
+            "§cVocê foi desconectado do servidor. Sua UUID está inválida." +
+            "\n" +
+            "§cAcesse: §nhttps://cmty.me/uuid §r§cpara mais informações.";
+
+    public static final String INVALID_USER = "\n" +
             "§c§lREDE COMMUNITY" +
             "\n" +
-            "§cYou're kicked from the server because your UUID is invalid" +
             "\n" +
-            "§cAccess: §nhttps://cmty.me/uuid §r§cfor more information.";
+            "§cOcorreu um erro ao carregar seu usuário de nosso banco de dados" +
+            "\n" +
+            "§cCódigo do erro: §aM4YSQL1" +
+            "\n" +
+            "§cCaso queira obter mais informações, utilize o código a cima" +
+            "\n" +
+            "§cpara conversar com algum membro de nossa equipe.";
 }
