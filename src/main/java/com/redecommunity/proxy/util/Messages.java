1a1e2fae58fee78c14681cef7a6089cb7acf79c7
Gutyerrez <srgutyerrez@gmail.com>
2019-12-19 05:48:59 -0300

    Create Messages.java

---
+++ src/main/java/com/redecommunity/proxy/util/Messages.java
@@ -0,0 +1,13 @@
+package com.redecommunity.proxy.util;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class Messages {
+    public static final String INVALID_UUID_MESSAGE = "\n" +
+            "§c§lREDE COMMUNITY" +
+            "\n" +
+            "§cYou're kicked from the server because your UUID is invalid" +
+            "\n" +
+            "§cAccess: §nhttps://cmty.me/uuid §r§cfor more information.";
+}
