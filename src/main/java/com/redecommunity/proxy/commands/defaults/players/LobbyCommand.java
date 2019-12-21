1d4e3c71a4f8ed76f1db126a465dcc92650f0bb7
Gutyerrez <srgutyerrez@gmail.com>
2019-12-21 06:04:29 -0300

    Create LobbyCommand.java

---
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/LobbyCommand.java
@@ -0,0 +1,19 @@
+package com.redecommunity.proxy.commands.defaults.players;
+
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class LobbyCommand extends CustomCommand {
+    public LobbyCommand() {
+        super("lobby", CommandRestriction.IN_GAME, null);
+    }
+
+    @Override
+    public void onCommand(User user, String[] strings) {
+
+    }
+}
