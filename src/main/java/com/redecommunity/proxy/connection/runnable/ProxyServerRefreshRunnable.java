46ec61a4dc14df7d16b73008d106496cab9333fd
Gutyerrez <srgutyerrez@gmail.com>
2019-12-21 06:23:29 -0300

    Update ProxyServerRefreshRunnable.java

--- src/main/java/com/redecommunity/proxy/connection/runnable/ProxyServerRefreshRunnable.java
+++ src/main/java/com/redecommunity/proxy/connection/runnable/ProxyServerRefreshRunnable.java
@@ -1,6 +1,6 @@
 package com.redecommunity.proxy.connection.runnable;
 
-import com.redecommunity.proxy.connection.dao.ProxyDao;
+import com.redecommunity.proxy.connection.dao.ProxyServerDao;
 import com.redecommunity.proxy.connection.data.ProxyServer;
 import com.redecommunity.proxy.connection.manager.ProxyServerManager;
 
@@ -10,9 +10,9 @@ import com.redecommunity.proxy.connection.manager.ProxyServerManager;
 public class ProxyServerRefreshRunnable implements Runnable {
     @Override
     public void run() {
-        ProxyDao proxyDao = new ProxyDao();
+        ProxyServerDao proxyServerDao = new ProxyServerDao();
 
-        proxyDao.findAll().forEach(proxyServer -> {
+        proxyServerDao.findAll().forEach(proxyServer -> {
             ProxyServer proxyServer1 = ProxyServerManager.getProxyServer(proxyServer.getId());
 
             if (proxyServer1 != null) proxyServer1.setUsersId(proxyServer.getUsersId());
