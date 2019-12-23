f7bacb0a916f561589940f5d449cf17489c3932e
Gutyerrez <srgutyerrez@gmail.com>
2019-12-23 12:55:37 -0300

    Update GroupRemoveCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/arguments/GroupRemoveCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/arguments/GroupRemoveCommand.java
@@ -62,6 +62,20 @@ public class GroupRemoveCommand extends CustomArgumentCommand {
 
         Group group = GroupManager.getGroup(targetGroupName);
 
+        if (group == null) {
+            user.sendMessage(
+                    language.getMessage("messages.default_commands.groups.unknown_group")
+            );
+            return;
+        }
+
+        if (!user.hasGroup(group)) {
+            user.sendMessage(
+                    language.getMessage("messages.default_commands.groups.insufficient_group")
+            );
+            return;
+        }
+
         Integer serverId = Integer.parseInt(targetServerPreId);
 
         Server server = ServerManager.getServer(serverId);
