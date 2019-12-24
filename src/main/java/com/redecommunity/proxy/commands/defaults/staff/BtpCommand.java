e62df45868bb40c33bd7dafb20259f79ff9e3405
Gutyerrez <srgutyerrez@gmail.com>
2019-12-24 11:37:26 -0300

    Update BtpCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/BtpCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/BtpCommand.java
@@ -1,7 +1,61 @@
 package com.redecommunity.proxy.commands.defaults.staff;
 
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.server.data.Server;
+
 /**
  * Created by @SrGutyerrez
  */
-public class BtpCommand {
+public class BtpCommand extends CustomCommand {
+    public BtpCommand() {
+        super("btp", CommandRestriction.IN_GAME, "administrator");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        if (args.length != 1) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            this.getName(),
+                            "<usuário>"
+                    )
+            );
+            return;
+        }
+
+        String targetName = args[0];
+
+        User user1 = UserManager.getUser(targetName);
+
+        if (user1 == null) {
+            user.sendMessage(
+                    language.getMessage("messages.player.invalid_player")
+            );
+            return;
+        }
+
+        if (!user1.isOnline()) {
+            user.sendMessage(
+                    language.getMessage("messages.player.player_offline")
+            );
+        }
+
+        Server server = user1.getServer();
+
+        user.sendMessage(
+                String.format(
+                        language.getMessage("btp"),
+                        user1.getDisplayName()
+                )
+        );
+
+        user.connect(server);
+    }
 }
