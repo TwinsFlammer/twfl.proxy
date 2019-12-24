c9eade3abb8a99d7e402c67208753f4a306c44a3
Gutyerrez <srgutyerrez@gmail.com>
2019-12-24 11:37:36 -0300

    Update AnnounceCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/AnnounceCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/AnnounceCommand.java
@@ -1,7 +1,31 @@
 package com.redecommunity.proxy.commands.defaults.staff;
 
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
 /**
  * Created by @SrGutyerrez
  */
-public class AnnounceCommand {
+public class AnnounceCommand extends CustomCommand {
+    public AnnounceCommand() {
+        super("alerta", CommandRestriction.ALL, "manager");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        if (args.length == 0) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            this.getName(),
+                            "<mensagem>"
+                    )
+            );
+            return;
+        }
+    }
 }
