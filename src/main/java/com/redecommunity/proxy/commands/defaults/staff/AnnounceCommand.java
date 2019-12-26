077f5a61c75c70333f829e89bb3b0b7dabb35fa2
Gutyerrez <srgutyerrez@gmail.com>
2019-12-26 15:35:31 -0300

    Update AnnounceCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/AnnounceCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/AnnounceCommand.java
@@ -2,8 +2,13 @@ package com.redecommunity.proxy.commands.defaults.staff;
 
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.api.bungeecord.util.JSONText;
 import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.util.Helper;
+import com.redecommunity.proxy.Proxy;
 
 /**
  * Created by @SrGutyerrez
@@ -27,5 +32,24 @@ public class AnnounceCommand extends CustomCommand {
             );
             return;
         }
+
+        String message = Helper.toMessage(args);
+
+        JSONText jsonText = new JSONText();
+
+        jsonText.next()
+                .text(user.isConsole() ? "§e[!]" : user.getPrefix() + user.getDisplayName())
+                .next()
+                .text("§f: ")
+                .next()
+                .text(message)
+                .next();
+
+        Group group = GroupManager.getGroup("default");
+
+        Proxy.getInstance().broadcastMessage(
+                group,
+                jsonText
+        );
     }
 }
