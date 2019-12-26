726d11a146a064d0f8a72a388cbb220cf2afad42
Gutyerrez <srgutyerrez@gmail.com>
2019-12-26 12:12:51 -0300

    Create BroadcastJedisMessageListener.java

---
+++ src/main/java/com/redecommunity/proxy/listeners/general/broadcast/BroadcastJedisMessageListener.java
@@ -0,0 +1,37 @@
+package com.redecommunity.proxy.listeners.general.broadcast;
+
+import com.redecommunity.api.bungeecord.util.JSONText;
+import com.redecommunity.common.shared.databases.redis.handler.JedisMessageListener;
+import com.redecommunity.common.shared.databases.redis.handler.annonation.ChannelName;
+import com.redecommunity.common.shared.databases.redis.handler.event.JedisMessageEvent;
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.util.Constants;
+import net.md_5.bungee.api.ProxyServer;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class BroadcastJedisMessageListener implements JedisMessageListener {
+    @ChannelName(name = Constants.BROADCAST_MESSAGE)
+    public void onMessage(JedisMessageEvent event) {
+        String message = event.getMessage();
+
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(message);
+
+        Integer groupId = ((Long) jsonObject.get("group_id")).intValue();
+        JSONText jsonText = (JSONText) jsonObject.get("message");
+
+        Group group = GroupManager.getGroup(groupId);
+
+        ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
+            User user = UserManager.getUser(proxiedPlayer.getUniqueId());
+
+            if (user.hasGroup(group)) jsonText.send(user);
+        });
+    }
+}
