75e78a163988918df494ab00a06aad941bf13c48
Gutyerrez <srgutyerrez@gmail.com>
2019-12-26 21:11:53 -0300

    Update BroadcastJedisMessageListener.java

--- src/main/java/com/redecommunity/proxy/listeners/general/broadcast/BroadcastJedisMessageListener.java
+++ src/main/java/com/redecommunity/proxy/listeners/general/broadcast/BroadcastJedisMessageListener.java
@@ -1,23 +1,14 @@
 package com.redecommunity.proxy.listeners.general.broadcast;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.redecommunity.api.bungeecord.util.JSONText;
 import com.redecommunity.common.shared.databases.redis.handler.JedisMessageListener;
 import com.redecommunity.common.shared.databases.redis.handler.annonation.ChannelName;
 import com.redecommunity.common.shared.databases.redis.handler.event.JedisMessageEvent;
-import com.redecommunity.common.shared.permissions.group.data.Group;
-import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import com.redecommunity.common.shared.util.Constants;
 import net.md_5.bungee.api.ProxyServer;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
  * Created by @SrGutyerrez
@@ -27,32 +18,15 @@ public class BroadcastJedisMessageListener implements JedisMessageListener {
     public void onMessage(JedisMessageEvent event) {
         String message = event.getMessage();
 
-        System.out.println(message);
-
         JSONObject jsonObject = (JSONObject) JSONValue.parse(message);
 
-        System.out.println(jsonObject);
-
         Integer groupId = ((Long) jsonObject.get("group_id")).intValue();
+        String value = (String) jsonObject.get("message");
 
-        try {
-            byte[] bytes = (byte[]) jsonObject.get("message");
+        ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
+            User user = UserManager.getUser(proxiedPlayer.getUniqueId());
 
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-
-            JSONText jsonText = (JSONText) objectInputStream.readObject();
-
-            Group group = GroupManager.getGroup(groupId);
-
-            ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
-                User user = UserManager.getUser(proxiedPlayer.getUniqueId());
-
-                if (user.hasGroup(group)) jsonText.send(user);
-            });
-        } catch (IOException | ClassNotFoundException exception) {
-            exception.printStackTrace();
-        }
+            if (user.hasGroup(groupId)) proxiedPlayer.sendMessage(value);
+        });
     }
 }
