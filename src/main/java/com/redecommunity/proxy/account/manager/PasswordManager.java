cb829321fcbcac662424686ae89ee9e990224dac
Gutyerrez <srgutyerrez@gmail.com>
2019-12-27 20:46:12 -0300

    Create PasswordManager.java

---
+++ src/main/java/com/redecommunity/proxy/account/manager/PasswordManager.java
@@ -0,0 +1,26 @@
+package com.redecommunity.proxy.account.manager;
+
+import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.account.data.Password;
+
+import java.util.HashMap;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PasswordManager {
+    private static HashMap<Integer, Password> passwords = Maps.newHashMap();
+
+    public static HashMap<Integer, Password> getPasswords() {
+        return PasswordManager.passwords;
+    }
+
+    public static Password getPasswords(Integer id) {
+        return PasswordManager.passwords.get(id);
+    }
+
+    public static Password getPasswords(User user) {
+        return PasswordManager.getPasswords(user.getId());
+    }
+}
