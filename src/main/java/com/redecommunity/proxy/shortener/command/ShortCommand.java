ac562a102f855893b4408d38a50cc37d7bead425
Gutyerrez <srgutyerrez@gmail.com>
2019-12-27 17:28:28 -0300

    Create ShortCommand.java

---
+++ src/main/java/com/redecommunity/proxy/shortener/command/ShortCommand.java
@@ -0,0 +1,71 @@
+package com.redecommunity.proxy.shortener.command;
+
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.util.Helper;
+import com.redecommunity.proxy.shortener.data.ShortedURL;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class ShortCommand extends CustomCommand {
+    public static final String COMMAND_NAME = "short";
+
+    public ShortCommand() {
+        super(ShortCommand.COMMAND_NAME, CommandRestriction.IN_GAME, "helper");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        if (args.length < 1 || args.length < 2) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            this.getName(),
+                            "<link>"
+                    )
+            );
+            return;
+        }
+
+        String link = args[0];
+
+        if (!Helper.isURLValid(link)) {
+            user.sendMessage(
+                    language.getMessage("shortener.invalid_url")
+            );
+            return;
+        }
+
+        String name = args.length == 2 ? args[1] : Helper.generateString(8);
+
+        if (args.length == 2) {
+            user.sendMessage(
+                    language.getMessage("shortener.too_many_arguments")
+            );
+            return;
+        }
+
+        ShortedURL shortedURL = new ShortedURL(
+                0,
+                link,
+                name,
+                user.getId(),
+                System.currentTimeMillis(),
+                true
+        );
+
+
+        user.sendMessage(
+                String.format(
+                        language.getMessage("shortener.shorted"),
+                        shortedURL.getUrl(),
+                        shortedURL.getName()
+                )
+        );
+    }
+}
