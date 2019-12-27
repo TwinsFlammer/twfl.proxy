acbb05bc731e4086f62e31f8ebff760facac038a
Gutyerrez <srgutyerrez@gmail.com>
2019-12-27 20:47:59 -0300

    Create AttemptManager.java

---
+++ src/main/java/com/redecommunity/proxy/account/manager/AttemptManager.java
@@ -0,0 +1,36 @@
+package com.redecommunity.proxy.account.manager;
+
+import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class AttemptManager {
+    private static HashMap<Integer, Integer> attempts = Maps.newHashMap();
+
+    public static HashMap<Integer, Integer> getAttempts() {
+        return AttemptManager.attempts;
+    }
+
+    public static Integer getPasswordAttempt(Integer id) {
+        return AttemptManager.attempts
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().equals(id))
+                .findFirst()
+                .map(Map.Entry::getValue)
+                .orElse(null);
+    }
+
+    public static Integer getPasswordAttempt(User user) {
+        return AttemptManager.getPasswordAttempt(user.getId());
+    }
+
+    public static Integer setPasswordAttempt(User user, Integer attempt) {
+        return AttemptManager.attempts.put(user.getId(), attempt);
+    }
+}
