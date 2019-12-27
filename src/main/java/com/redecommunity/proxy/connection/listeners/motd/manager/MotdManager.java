256cf5d66981a461e89242acebaa9a440629d65b
Gutyerrez <srgutyerrez@gmail.com>
2019-12-26 21:32:21 -0300

    Update MotdManager.java

--- src/main/java/com/redecommunity/proxy/connection/listeners/motd/manager/MotdManager.java
+++ src/main/java/com/redecommunity/proxy/connection/listeners/motd/manager/MotdManager.java
@@ -1,17 +1,27 @@
 package com.redecommunity.proxy.connection.listeners.motd.manager;
 
 import com.google.common.collect.Lists;
+import com.redecommunity.proxy.connection.listeners.motd.dao.MotdDao;
 import com.redecommunity.proxy.connection.listeners.motd.data.Motd;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by @SrGutyerrez
  */
 public class MotdManager {
     private static List<Motd> motds = Lists.newArrayList();
+
+    public MotdManager() {
+        MotdDao motdDao = new MotdDao();
+
+        Set<Motd> motds = motdDao.findAll();
+
+        MotdManager.motds.addAll(motds);
+    }
 
     public static Motd getCurrentMotd() {
         return MotdManager.motds
