2c41821e24113420a6e7e89a8b88837ebe12e6ae
Gutyerrez <srgutyerrez@gmail.com>
2019-12-29 19:16:02 -0300

    Create FriendCancelCommand.java

---
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendCancelCommand.java
@@ -0,0 +1,18 @@
+package com.redecommunity.proxy.commands.defaults.players.friend.arguments;
+
+import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class FriendCancelCommand extends CustomArgumentCommand {
+    public FriendCancelCommand() {
+        super(0, "cancelar");
+    }
+
+    @Override
+    public void onCommand(User user, String[] strings) {
+
+    }
+}
