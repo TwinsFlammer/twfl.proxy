2c7a14e1d4437889332c2f54f60a6a776c14bd97
Gutyerrez <srgutyerrez@gmail.com>
2019-12-29 19:16:15 -0300

    Create FriendAddCommand.java

---
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendAddCommand.java
@@ -0,0 +1,50 @@
+package com.redecommunity.proxy.commands.defaults.players.friend.arguments;
+
+import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.proxy.commands.defaults.players.friend.FriendCommand;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class FriendAddCommand extends CustomArgumentCommand {
+    public FriendAddCommand() {
+        super(0, "add");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        if (args.length != 1) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            FriendCommand.COMMAND_NAME,
+                            "add <usuário>"
+                    )
+            );
+            return;
+        }
+
+        String targetName = args[0];
+
+        User user1 = UserManager.getUser(targetName);
+
+        if (user1 == null) {
+            user.sendMessage(
+                    language.getMessage("messages.player.invalid_player")
+            );
+            return;
+        }
+
+        if (!user1.isOnline()) {
+            user.sendMessage(
+                    language.getMessage("messages.player.player_offline")
+            );
+            return;
+        }
+    }
+}
