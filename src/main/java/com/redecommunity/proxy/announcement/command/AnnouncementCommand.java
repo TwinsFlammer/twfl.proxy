235ed8e5d63c34f40084b3f33f169426405edb06
Gutyerrez <srgutyerrez@gmail.com>
2019-12-30 01:52:55 -0300

    Update AnnouncementCommand.java

--- src/main/java/com/redecommunity/proxy/announcement/command/AnnouncementCommand.java
+++ src/main/java/com/redecommunity/proxy/announcement/command/AnnouncementCommand.java
@@ -1,7 +1,35 @@
 package com.redecommunity.proxy.announcement.command;
 
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.announcement.command.arguments.AnnouncementDisableCommand;
+import com.redecommunity.proxy.announcement.command.arguments.AnnouncementEnableCommand;
+import com.redecommunity.proxy.announcement.command.arguments.AnnouncementListCommand;
+
 /**
  * Created by @SrGutyerrez
  */
-public class AnnouncementCommand {
+public class AnnouncementCommand extends CustomCommand {
+    public static final String COMMAND_NAME = "anuncio";
+
+    public AnnouncementCommand() {
+        super(AnnouncementCommand.COMMAND_NAME, CommandRestriction.IN_GAME, "manager");
+
+        this.addArgument(
+                new AnnouncementDisableCommand(),
+                new AnnouncementEnableCommand(),
+                new AnnouncementListCommand()
+        );
+    }
+
+    @Override
+    public void onCommand(User user, String[] strings) {
+        Language language = user.getLanguage();
+
+        user.sendMessage(
+                language.getMessage("announcement.usage")
+        );
+    }
 }
