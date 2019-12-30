a1e563d30d1526c0c420f3be92d8ff1b7ef16a15
Gutyerrez <srgutyerrez@gmail.com>
2019-12-30 01:26:46 -0300

    Create Announcement.java

---
+++ src/main/java/com/redecommunity/proxy/announcement/data/Announcement.java
@@ -0,0 +1,26 @@
+package com.redecommunity.proxy.announcement.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.net.URL;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@AllArgsConstructor
+public class Announcement {
+    @Getter
+    private final Integer id;
+    @Getter
+    private final String title, message;
+    @Getter
+    private final URL url;
+    @Setter
+    private Boolean active;
+
+    public Boolean isActive() {
+        return this.active;
+    }
+}
