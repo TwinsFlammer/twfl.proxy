df3eec10705ce53eff97a2bae116bc7075717168
Gutyerrez <srgutyerrez@gmail.com>
2019-12-30 00:42:54 -0300

    Update FriendCancelCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendCancelCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendCancelCommand.java
@@ -1,7 +1,10 @@
 package com.redecommunity.proxy.commands.defaults.players.friend.arguments;
 
 import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.proxy.commands.defaults.players.friend.FriendCommand;
 
 /**
  * Created by @SrGutyerrez
@@ -12,7 +15,40 @@ public class FriendCancelCommand extends CustomArgumentCommand {
     }
 
     @Override
-    public void onCommand(User user, String[] strings) {
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
 
+        if (args.length != 1) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            FriendCommand.COMMAND_NAME,
+                            "add <usuário>"
+                    )
+            );
+            return;
+        }
+
+        String targetName = args[0];
+
+        User user1 = UserManager.getUser(targetName);
+
+        if (user1 == null) {
+            user.sendMessage(
+                    language.getMessage("messages.player.invalid_player")
+            );
+            return;
+        }
+
+        if (!user.isFriend(user1)) {
+
+            return;
+        }
+
+        user.removeFriend(user1);
+
+        user.sendMessage(
+                language.getMessage("friends.request_cancelled")
+        );
     }
 }
