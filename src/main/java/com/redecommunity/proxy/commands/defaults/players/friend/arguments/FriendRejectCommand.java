3bcf0d16f32842afc68aae0519473cc27c075ed1
Gutyerrez <srgutyerrez@gmail.com>
2019-12-30 00:43:37 -0300

    Update FriendRejectCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendRejectCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/friend/arguments/FriendRejectCommand.java
@@ -1,7 +1,10 @@
 package com.redecommunity.proxy.commands.defaults.players.friend.arguments;
 
 import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.proxy.commands.defaults.players.friend.FriendCommand;
 
 /**
  * Created by @SrGutyerrez
@@ -12,7 +15,57 @@ public class FriendRejectCommand extends CustomArgumentCommand {
     }
 
     @Override
-    public void onCommand(User user, String[] strings) {
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
 
+        if (args.length != 1) {
+            user.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            FriendCommand.COMMAND_NAME,
+                            "aceitar <usuário>"
+                    )
+            );
+            return;
+        }
+
+        String targetName = args[0];
+
+        User user1 = UserManager.getUser(targetName);
+
+        if (user1 == null) {
+            user.sendMessage(
+                    language.getMessage("messages.player.invalid_player")
+            );
+            return;
+        }
+
+        if (user.isFriend(user1) && user1.isFriend(user)) {
+            user.sendMessage(
+                    language.getMessage("friends.already_friend")
+            );
+            return;
+        }
+
+        if (user.isFriend(user1) && !user1.isFriend(user)) {
+            user.sendMessage(
+                    language.getMessage("friends.not_invited_friend_to_you")
+            );
+            return;
+        }
+
+        user.sendMessage(
+                String.format(
+                        language.getMessage("friends.rejected"),
+                        user1.getDisplayName()
+                )
+        );
+
+        user1.sendMessage(
+                String.format(
+                        language.getMessage("friends.invite_rejected"),
+                        user.getDisplayName()
+                )
+        );
     }
 }
