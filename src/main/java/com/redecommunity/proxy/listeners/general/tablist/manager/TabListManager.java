e41d203846f32275d3cf93b8f3e16312d58172a2
Gutyerrez <srgutyerrez@gmail.com>
2019-12-31 19:54:38 -0300

    Update TabListManager.java

--- src/main/java/com/redecommunity/proxy/listeners/general/tablist/manager/TabListManager.java
+++ src/main/java/com/redecommunity/proxy/listeners/general/tablist/manager/TabListManager.java
@@ -1,19 +1,45 @@
 package com.redecommunity.proxy.listeners.general.tablist.manager;
 
 import com.google.common.collect.Lists;
+import com.redecommunity.common.shared.Common;
 import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.listeners.general.tablist.dao.TabListDao;
 import com.redecommunity.proxy.listeners.general.tablist.data.TabList;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by @SrGutyerrez
  */
 public class TabListManager {
     private static List<TabList> tabLists = Lists.newArrayList();
+
+    public TabListManager() {
+        TabListDao tabListDao = new TabListDao();
+
+        List<TabList> tabLists = Lists.newArrayList(tabListDao.findAll());
+
+        TabListManager.setTabLists(tabLists);
+
+        Common.getInstance().getScheduler().scheduleAtFixedRate(
+                () -> {
+                    List<TabList> tabLists1 = Lists.newArrayList(tabListDao.findAll());
+
+                    TabListManager.setTabLists(tabLists1);
+                },
+                0,
+                20,
+                TimeUnit.SECONDS
+        );
+    }
+
+    private static List<TabList> setTabLists(List<TabList> tabLists) {
+        return TabListManager.tabLists = tabLists;
+    }
 
     public static TabList getCurrentTabList(User user) {
         return TabListManager.tabLists
@@ -24,7 +50,7 @@ public class TabListManager {
                 .orElse(null);
     }
 
-    public static TabList getStaffTaList(User user) {
+    public static TabList getStaffTabList(User user) {
         return TabListManager.tabLists
                 .stream()
                 .filter(TabList::isActive)
