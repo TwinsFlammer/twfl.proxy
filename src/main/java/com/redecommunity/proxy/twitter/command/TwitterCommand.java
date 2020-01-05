fce18291cf71a035c886cd2195c332a6fe1e2103
Gutyerrez <srgutyerrez@gmail.com>
2020-01-05 04:49:22 -0300

    Create TwitterCommand.java

---
+++ src/main/java/com/redecommunity/proxy/twitter/command/TwitterCommand.java
@@ -0,0 +1,36 @@
+package com.redecommunity.proxy.twitter.command;
+
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.twitter.command.arguments.*;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class TwitterCommand extends CustomCommand {
+    public static final String COMMAND_NAME = "twitter";
+
+    public TwitterCommand() {
+        super(TwitterCommand.COMMAND_NAME, CommandRestriction.IN_GAME, "default");
+
+        this.addArgument(
+                new AssociateCommand(),
+                new DisassociateCommand(),
+                new FollowCommand(),
+                new FollowMastersCommand(),
+                new InfoCommand(),
+                new TweetCommand()
+        );
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        user.sendMessage(
+                language.getMessage("twitter.usage")
+        );
+    }
+}
