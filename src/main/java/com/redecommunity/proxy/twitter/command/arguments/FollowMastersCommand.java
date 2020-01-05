07fd2d4fe999e1a620652ba00c5f18853650c8dd
Gutyerrez <srgutyerrez@gmail.com>
2020-01-05 04:51:42 -0300

    Create FollowMastersCommand.java

---
+++ src/main/java/com/redecommunity/proxy/twitter/command/arguments/FollowMastersCommand.java
@@ -0,0 +1,18 @@
+package com.redecommunity.proxy.twitter.command.arguments;
+
+import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class FollowMastersCommand extends CustomArgumentCommand {
+    public FollowMastersCommand() {
+        super(0, "seguirmasters");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+
+    }
+}
