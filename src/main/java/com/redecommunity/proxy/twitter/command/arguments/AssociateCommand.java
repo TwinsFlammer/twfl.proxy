b53e647193393469bd27dd914d3912178d9617bb
Gutyerrez <srgutyerrez@gmail.com>
2020-01-05 04:50:21 -0300

    Create AssociateCommand.java

---
+++ src/main/java/com/redecommunity/proxy/twitter/command/arguments/AssociateCommand.java
@@ -0,0 +1,18 @@
+package com.redecommunity.proxy.twitter.command.arguments;
+
+import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class AssociateCommand extends CustomArgumentCommand {
+    public AssociateCommand() {
+        super(0, "associar");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+
+    }
+}
