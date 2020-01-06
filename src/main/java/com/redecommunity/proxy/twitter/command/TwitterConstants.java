b144b41c294d517292b8289766806f60e14ecc78
Gutyerrez <srgutyerrez@gmail.com>
2020-01-05 21:47:46 -0300

    Create TwitterConstants.java

---
+++ src/main/java/com/redecommunity/proxy/twitter/command/TwitterConstants.java
@@ -0,0 +1,11 @@
+package com.redecommunity.proxy.twitter.command;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class TwitterConstants {
+    public static final String[] MASTER_USERS = new String[] {
+            "SrGutyerrez",
+            "MrSkyzin"
+    };
+}
