d6b61100065f91c2b75213f9e9f7371de37b2d23
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 09:45:26 -0300

    Update RegisterCommand.java

--- src/main/java/com/redecommunity/proxy/authentication/commands/RegisterCommand.java
+++ src/main/java/com/redecommunity/proxy/authentication/commands/RegisterCommand.java
@@ -1,4 +1,4 @@
-package com.redecommunity.proxy.account.commands;
+package com.redecommunity.proxy.authentication.commands;
 
 import com.google.common.collect.Maps;
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
