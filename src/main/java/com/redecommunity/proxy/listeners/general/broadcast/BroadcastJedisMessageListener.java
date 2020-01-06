dd99873aa0d932eba653a823b38800680d5029a2
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 07:05:19 -0300

    Update BroadcastJedisMessageListener.java

--- src/main/java/com/redecommunity/proxy/listeners/general/broadcast/BroadcastJedisMessageListener.java
+++ src/main/java/com/redecommunity/proxy/listeners/general/broadcast/BroadcastJedisMessageListener.java
@@ -5,8 +5,10 @@ import com.redecommunity.common.shared.databases.redis.handler.annonation.Channe
 import com.redecommunity.common.shared.databases.redis.handler.event.JedisMessageEvent;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.preference.Preference;
 import com.redecommunity.common.shared.util.Constants;
 import net.md_5.bungee.api.ProxyServer;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -23,10 +25,32 @@ public class BroadcastJedisMessageListener implements JedisMessageListener {
         Integer groupId = ((Long) jsonObject.get("group_id")).intValue();
         String value = (String) jsonObject.get("message");
 
+        JSONArray jsonArray = (JSONArray) jsonObject.get("preferences");
+
+        Preference[] preferences = new Preference[jsonArray.size()];
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            Object object = jsonArray.get(i);
+
+            String preferenceName = (String) object;
+
+            Preference preference = Preference.valueOf(preferenceName);
+
+            preferences[i] = preference;
+        }
+
         ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
             User user = UserManager.getUser(proxiedPlayer.getUniqueId());
 
-            if (user.hasGroup(groupId) && user.isLogged()) proxiedPlayer.sendMessage(value);
+            if (user.hasGroup(groupId) && user.isLogged()) {
+
+                Integer disabledCount = 0;
+
+                for (Preference preference : preferences)
+                    if (user.isDisabled(preference)) disabledCount++;
+
+                if (disabledCount == 0) proxiedPlayer.sendMessage(value);
+            }
         });
     }
 }
