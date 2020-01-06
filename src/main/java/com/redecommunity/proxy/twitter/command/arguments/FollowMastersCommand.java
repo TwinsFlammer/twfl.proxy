ac230e233ed2dcdd098884f8fcd09c072455dea9
Gutyerrez <srgutyerrez@gmail.com>
2020-01-05 21:52:26 -0300

    Update FollowMastersCommand.java

--- src/main/java/com/redecommunity/proxy/twitter/command/arguments/FollowMastersCommand.java
+++ src/main/java/com/redecommunity/proxy/twitter/command/arguments/FollowMastersCommand.java
@@ -1,7 +1,13 @@
 package com.redecommunity.proxy.twitter.command.arguments;
 
 import com.redecommunity.api.bungeecord.commands.CustomArgumentCommand;
+import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.twitter.command.TwitterConstants;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.Arrays;
 
 /**
  * Created by @SrGutyerrez
@@ -13,6 +19,32 @@ public class FollowMastersCommand extends CustomArgumentCommand {
 
     @Override
     public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
 
+        if (!user.isTwitterAssociated()) {
+            user.sendMessage(
+                    language.getMessage("twitter.not_associated")
+            );
+            return;
+        }
+
+        Twitter twitter = user.getTwitter();
+
+        Arrays.stream(TwitterConstants.MASTER_USERS).forEach(masterUser -> {
+            try {
+                twitter.createFriendship(masterUser);
+            } catch (TwitterException exception) {
+                user.sendMessage(
+                        String.format(
+                                language.getMessage("twitter.followed_user"),
+                                masterUser
+                        )
+                );
+            }
+        });
+
+        user.sendMessage(
+                language.getMessage("twitter.followed_masters")
+        );
     }
 }
