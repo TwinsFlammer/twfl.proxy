ca97208165c4b3202f437e7435b8d2d7c7acf280
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 23:57:37 -0300

    Update PunishmentManager.java

--- src/main/java/com/redecommunity/proxy/punish/manager/PunishmentManager.java
+++ src/main/java/com/redecommunity/proxy/punish/manager/PunishmentManager.java
@@ -1,5 +1,7 @@
 package com.redecommunity.proxy.punish.manager;
 
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.punish.data.PunishReason;
 import com.redecommunity.proxy.punish.data.Punishment;
 
 import java.sql.ResultSet;
@@ -9,14 +11,33 @@ import java.sql.SQLException;
  * Created by @SrGutyerrez
  */
 public class PunishmentManager {
+    public static Punishment generatePunishment(User staffer, User user, PunishReason punishReason, String proof, Boolean hidden) {
+        return new Punishment(
+                0,
+                user.getId(),
+                staffer.getId(),
+                punishReason.getId(),
+                null,
+                null,
+                hidden,
+                null,
+                true,
+                proof,
+                System.currentTimeMillis(),
+                null,
+                null,
+                null
+        );
+    }
+
     public static Punishment toPunishment(ResultSet resultSet) throws SQLException {
         return new Punishment(
                 resultSet.getInt("id"),
                 resultSet.getInt("user_id"),
                 resultSet.getInt("staffer_id"),
-                resultSet.getInt("motive_id"),
+                resultSet.getInt("reason_id"),
                 resultSet.getInt("revoker_id"),
-                resultSet.getInt("revoker_motive_id"),
+                resultSet.getInt("revoke_reason_id"),
                 resultSet.getBoolean("hidden"),
                 resultSet.getBoolean("perpetual"),
                 resultSet.getBoolean("status"),
