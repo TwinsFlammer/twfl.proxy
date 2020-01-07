2660c808dcb7e0728898d65ff670547b1359032a
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 22:20:27 -0300

    Update RevokeMotive.java

--- src/main/java/com/redecommunity/proxy/punish/data/RevokeMotive.java
+++ src/main/java/com/redecommunity/proxy/punish/data/RevokeMotive.java
@@ -1,7 +1,16 @@
 package com.redecommunity.proxy.punish.data;
 
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by @SrGutyerrez
  */
+@RequiredArgsConstructor
+@Getter
 public class RevokeMotive {
+    private final Integer id;
+    private final String name, displayName, description;
+    private final Group group;
 }
