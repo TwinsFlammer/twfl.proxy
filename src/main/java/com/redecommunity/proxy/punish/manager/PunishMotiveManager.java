c09b7808409bc4041a26f49871520503a3186213
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 22:21:08 -0300

    Create PunishMotiveManager.java

---
+++ src/main/java/com/redecommunity/proxy/punish/manager/PunishMotiveManager.java
@@ -0,0 +1,80 @@
+package com.redecommunity.proxy.punish.manager;
+
+import com.google.common.collect.Lists;
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
+import com.redecommunity.proxy.punish.data.Duration;
+import com.redecommunity.proxy.punish.data.PunishMotive;
+import com.redecommunity.proxy.punish.data.enums.PunishType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PunishMotiveManager {
+    private static List<PunishMotive> punishMotives = Lists.newArrayList();
+
+    public static List<PunishMotive> getPunishMotives() {
+        return PunishMotiveManager.punishMotives
+                .stream()
+                .sorted((punishMotive1, punishMotive2) -> punishMotive2.getName().compareTo(punishMotive1.getName()))
+                .collect(Collectors.toList());
+    }
+
+    public static PunishMotive getPunishMotive(String name) {
+        return PunishMotiveManager.punishMotives
+                .stream()
+                .filter(punishMotive -> punishMotive.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static PunishMotive toMotive(ResultSet resultSet) throws SQLException {
+        String name = resultSet.getString("name");
+
+        Integer groupId = resultSet.getInt("group_id");
+
+        Group group = GroupManager.getGroup(groupId);
+
+        Integer duration = resultSet.getInt("duration");
+        String preTimeUnit = resultSet.getString("duration_type");
+        String type = resultSet.getString("type");
+
+        TimeUnit timeType = TimeUnit.valueOf(preTimeUnit);
+        PunishType punishType = PunishType.valueOf(type);
+
+        Duration duration1 = new Duration(duration, timeType, punishType);
+
+        List<Duration> durations = Lists.newArrayList();
+
+        durations.add(duration1);
+
+        Optional<PunishMotive> similarMotive = PunishMotiveManager.getSimilarPunishMotive(name);
+
+        PunishMotive punishMotive = similarMotive.isPresent() ? PunishMotiveManager.getPunishMotive(name) : new PunishMotive(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("display_name"),
+                resultSet.getString("description"),
+                group,
+                durations
+        );
+
+        if (similarMotive.isPresent()) punishMotive.getDurations().add(duration1);
+
+        return punishMotive;
+    }
+
+    private static Optional<PunishMotive> getSimilarPunishMotive(String name) {
+        return PunishMotiveManager.punishMotives
+                .stream()
+                .filter(punishMotive -> punishMotive.getName().equalsIgnoreCase(name))
+                .findFirst();
+    }
+}
