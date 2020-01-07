5816de99340089f8f142dbfc82510bd3c4cb4998
Gutyerrez <srgutyerrez@gmail.com>
2020-01-07 00:12:35 -0300

    Update Punishment.java

--- src/main/java/com/redecommunity/proxy/punish/data/Punishment.java
+++ src/main/java/com/redecommunity/proxy/punish/data/Punishment.java
@@ -1,9 +1,14 @@
 package com.redecommunity.proxy.punish.data;
 
+import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.proxy.punish.dao.PunishmentDao;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.md_5.bungee.api.ChatColor;
+
+import java.util.HashMap;
 
 /**
  * Created by @SrGutyerrez
@@ -14,9 +19,9 @@ public class Punishment {
     private final Integer id,
             userId,
             stafferId,
-            motiveId,
+            reasonId,
             revokerId,
-            revokeMotiveId;
+            revokeReasonId;
 
     private final Boolean hidden,
             perpetual;
@@ -42,17 +47,52 @@ public class Punishment {
         return this.perpetual;
     }
 
-    public Boolean isActive() {
-        if (System.currentTimeMillis() >= this.endTime) {
-            this.status = false;
-            // TODO not implemented yet
-        }
+    public Boolean isTemporary() {
+        return this.endTime != null;
+    }
 
+    public Boolean isActive() {
+        if (this.isTemporary() && System.currentTimeMillis() >= this.endTime) {
+            this.status = false;
+
+            HashMap<String, Object> keys = Maps.newHashMap();
+
+            keys.put("status", this.status);
+
+            PunishmentDao punishmentDao = new PunishmentDao();
+
+            punishmentDao.update(
+                    keys,
+                    "id",
+                    this.id
+            );
+        }
         return this.status;
     }
 
     public ChatColor getColor() {
         if (this.revokerId != null) return ChatColor.GRAY;
         return this.startTime == null ? ChatColor.GOLD : this.status ? ChatColor.GREEN : ChatColor.RED;
+    }
+
+    public void revoke(User user, RevokeReason revokeReason) {
+
+    }
+
+    public void broadcast() {
+        HashMap<String, Object> keys = Maps.newHashMap();
+
+        keys.put("status", this.status);
+        keys.put("revoker_id", this.revokerId);
+        keys.put("revoke_motive_id", this.revokeReasonId);
+        keys.put("revoke_time", this.revokeTime);
+
+        PunishmentDao punishmentDao = new PunishmentDao();
+
+        punishmentDao.update(
+                keys,
+                "id",
+                this.id
+        );
     }
 }
