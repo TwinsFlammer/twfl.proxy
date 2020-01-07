b11dd0669c3d25d8e00bac667c471c9aeaeabb2c
Gutyerrez <srgutyerrez@gmail.com>
2020-01-07 01:40:33 -0300

    Update Punishment.java

--- src/main/java/com/redecommunity/proxy/punish/data/Punishment.java
+++ src/main/java/com/redecommunity/proxy/punish/data/Punishment.java
@@ -1,14 +1,22 @@
 package com.redecommunity.proxy.punish.data;
 
 import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.proxy.Proxy;
 import com.redecommunity.proxy.punish.dao.PunishmentDao;
+import com.redecommunity.proxy.punish.manager.PunishReasonManager;
+import com.redecommunity.proxy.punish.manager.PunishmentManager;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.md_5.bungee.api.ChatColor;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by @SrGutyerrez
@@ -19,8 +27,10 @@ public class Punishment {
     private final Integer id,
             userId,
             stafferId,
-            reasonId,
-            revokerId,
+            reasonId;
+
+    @Getter
+    private Integer revokeUserId,
             revokeReasonId;
 
     private final Boolean hidden,
@@ -36,8 +46,10 @@ public class Punishment {
     @Getter
     private final Long time,
             startTime,
-            endTime,
-            revokeTime;
+            endTime;
+
+    @Getter
+    private Long revokeTime;
 
     public Boolean isHidden() {
         return this.hidden;
@@ -71,20 +83,42 @@ public class Punishment {
     }
 
     public ChatColor getColor() {
-        if (this.revokerId != null) return ChatColor.GRAY;
+        if (this.revokeUserId != null) return ChatColor.GRAY;
         return this.startTime == null ? ChatColor.GOLD : this.status ? ChatColor.GREEN : ChatColor.RED;
     }
 
-    public void revoke(User user, RevokeReason revokeReason) {
-
+    public PunishReason getPunishReason() {
+        return PunishReasonManager.getPunishReason(this.reasonId);
     }
 
-    public void broadcast() {
+    private Duration getDuration() {
+        PunishReason punishReason = this.getPunishReason();
+
+        List<Punishment> punishments = PunishmentManager.getPunishments(this.userId);
+
+        Integer count = (int) punishments.stream()
+                .filter(Objects::nonNull)
+                .filter(punishment -> punishment.getPunishReason().isSimilar(punishReason))
+                .count();
+
+        List<Duration> durations = punishReason.getDurations();
+
+        Duration duration = durations.get((durations.size() < count ? durations.size() : count) - 1);
+
+        return duration;
+    }
+
+    public void revoke(User user, RevokeReason revokeReason) {
+        this.status = false;
+        this.revokeUserId = user.getId();
+        this.revokeReasonId = revokeReason.getId();
+        this.revokeTime = System.currentTimeMillis();
+
         HashMap<String, Object> keys = Maps.newHashMap();
 
         keys.put("status", this.status);
-        keys.put("revoker_id", this.revokerId);
-        keys.put("revoke_motive_id", this.revokeReasonId);
+        keys.put("revoke_user_id", this.revokeUserId);
+        keys.put("revoke_reason_id", this.revokeReasonId);
         keys.put("revoke_time", this.revokeTime);
 
         PunishmentDao punishmentDao = new PunishmentDao();
@@ -93,6 +127,40 @@ public class Punishment {
                 keys,
                 "id",
                 this.id
+        );
+    }
+
+    public void broadcast() {
+        User user = UserManager.getUser(this.userId),
+                staffer = UserManager.getUser(this.stafferId);
+
+        PunishReason punishReason = this.getPunishReason();
+        Duration duration = this.getDuration();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("\n")
+                .append("§c * ")
+                .append(user.getDisplayName())
+                .append(" foi ")
+                .append(duration.getPunishType().getDisplayName())
+                .append(" por ")
+                .append(staffer.getDisplayName())
+                .append("\n")
+                .append("§c * Motivo: ")
+                .append(punishReason.getDisplayName())
+                .append(this.proof == null || this.proof.isEmpty() ? " - " + this.proof : "")
+                .append("\n");
+
+        if (duration.isTemporary()) stringBuilder.append("§c * Duração: ")
+                .append(duration.getTimeTypeDisplayName())
+                .append("\n");
+
+        Group group = this.hidden ? GroupManager.getGroup("manager") : GroupManager.getGroup("default");
+
+        Proxy.broadcastMessage(
+                group,
+                stringBuilder.toString()
         );
     }
 }
