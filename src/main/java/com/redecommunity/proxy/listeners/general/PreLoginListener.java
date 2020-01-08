be812e3ed3021ff1f3c95c4665bc682177aab36b
Gutyerrez <srgutyerrez@gmail.com>
2020-01-08 02:57:20 -0300

    Create PreLoginListener.java

---
+++ src/main/java/com/redecommunity/proxy/listeners/general/PreLoginListener.java
@@ -0,0 +1,28 @@
+package com.redecommunity.proxy.listeners.general;
+
+import net.md_5.bungee.api.connection.Connection;
+import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.event.PreLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+import java.util.UUID;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PreLoginListener implements Listener {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onLogin(PreLoginEvent event) {
+        PendingConnection pendingConnection = event.getConnection();
+
+        UUID uniqueId = pendingConnection.getUniqueId();
+        String uuid = pendingConnection.getUUID();
+        String playerName = pendingConnection.getName();
+
+        System.out.println("UniqueID: " + uniqueId);
+        System.out.println("UUID: " + uuid);
+        System.out.println("PlayerName: " + playerName);
+    }
+}
