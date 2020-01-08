8224570f0f48a9a6d2470bbb1a96b966bd64056f
Gutyerrez <srgutyerrez@gmail.com>
2020-01-08 10:59:17 -0300

    Create DiscordCommand.java

---
+++ src/main/java/com/redecommunity/proxy/commands/defaults/players/discord/DiscordCommand.java
@@ -0,0 +1,27 @@
+package com.redecommunity.proxy.commands.defaults.players.discord;
+
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
+import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class DiscordCommand extends CustomCommand {
+    public DiscordCommand() {
+        super("discord", CommandRestriction.IN_GAME, "default");
+    }
+
+    @Override
+    public void onCommand(User user, String[] args) {
+        Language language = user.getLanguage();
+
+        if (user.getDiscordId() != null && user.getDiscordId() != 0) {
+            user.sendMessage(
+                    language.getMessage("discord.associated")
+            );
+            return;
+        }
+    }
+}
