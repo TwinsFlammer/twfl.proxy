9199e51c835c99075a15eda7d507a46bd433d20d
Gutyerrez <srgutyerrez@gmail.com>
2020-01-08 03:19:59 -0300

    Update PreLoginListener.java

--- src/main/java/com/redecommunity/proxy/listeners/general/PreLoginListener.java
+++ src/main/java/com/redecommunity/proxy/listeners/general/PreLoginListener.java
@@ -1,12 +1,25 @@
 package com.redecommunity.proxy.listeners.general;
 
+import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.user.dao.UserDao;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.proxy.punish.data.Duration;
+import com.redecommunity.proxy.punish.data.PunishReason;
+import com.redecommunity.proxy.punish.data.Punishment;
+import com.redecommunity.proxy.punish.data.enums.PunishType;
+import com.redecommunity.proxy.punish.manager.PunishmentManager;
+import com.redecommunity.proxy.util.Messages;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
+import org.apache.commons.io.Charsets;
 
+import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 /**
  * Created by @SrGutyerrez
@@ -16,10 +29,70 @@ public class PreLoginListener implements Listener {
     public void onLogin(PreLoginEvent event) {
         PendingConnection pendingConnection = event.getConnection();
 
-        UUID uniqueId = pendingConnection.getUniqueId();
-        String playerName = pendingConnection.getName();
+        User user = UserManager.getUser(pendingConnection.getName());
 
-        System.out.println("UniqueID: " + uniqueId);
-        System.out.println("PlayerName: " + playerName);
+        if (user == null) {
+            UserDao userDao = new UserDao();
+
+            user = UserManager.generateUser(
+                    pendingConnection.getName(),
+                    this.generateUUID(pendingConnection.getName())
+            );
+
+            userDao.insert(user);
+        }
+
+        user = UserManager.getUser(pendingConnection.getName());
+
+        if (user == null) {
+            event.setCancelReason(Messages.INVALID_USER);
+            event.setCancelled(true);
+            return;
+        }
+
+        Punishment punishment = PunishmentManager.getPunishments(user)
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(Punishment::isActive)
+                .filter(Punishment::isBan)
+                .findFirst()
+                .orElse(null);
+
+        PunishmentManager.getPunishments(user)
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(this.predicate(Punishment::isStarted).negate())
+                .forEach(Punishment::start);
+
+        if (punishment != null) {
+            Language language = user.getLanguage();
+
+            Duration duration = punishment.getDuration();
+            PunishType punishType = duration.getPunishType();
+            PunishReason punishReason = punishment.getPunishReason();
+
+            event.setCancelReason(
+                    String.format(
+                            language.getMessage("punishment.kick_message"),
+                            punishType.getDisplayName(),
+                            punishReason.getDisplayName(),
+                            punishment.getProof(),
+                            punishment.getStaffer().getDisplayName(),
+                            punishment.getId()
+                    )
+            );
+            event.setCancelled(true);
+            return;
+        }
+
+
+    }
+
+    private <T> Predicate<T> predicate(Predicate<T> predicate) {
+        return predicate;
+    }
+
+    private UUID generateUUID(String username) {
+        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + username).getBytes(Charsets.UTF_8));
     }
 }
