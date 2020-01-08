4cf2b852dd070ed5e931e2c0ba4f8aeed60ce87d
Gutyerrez <srgutyerrez@gmail.com>
2020-01-08 06:38:13 -0300

    Update AnnouncementManager.java

--- src/main/java/com/redecommunity/proxy/announcement/manager/AnnouncementManager.java
+++ src/main/java/com/redecommunity/proxy/announcement/manager/AnnouncementManager.java
@@ -14,27 +14,18 @@ import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by @SrGutyerrez
  */
 public class AnnouncementManager {
-    public static final String CHANNEL_NAME = "announcement_channel";
-
-    private static List<Announcement> announcements = Lists.newArrayList();
-
     public AnnouncementManager() {
         AnnouncementDao announcementDao = new AnnouncementDao();
 
-        Set<Announcement> announcements = announcementDao.findAll();
-
-        AnnouncementManager.announcements.addAll(announcements);
-
         Common.getInstance().getScheduler().scheduleWithFixedDelay(
                 () -> {
-                    if (AnnouncementManager.announcements.isEmpty()) return;
+                    List<Announcement> announcements = Lists.newArrayList(announcementDao.findAll());
 
                     JSONText jsonText = new JSONText();
 
@@ -46,22 +37,24 @@ public class AnnouncementManager {
                             .text("\n\n")
                             .next();
 
-                    AnnouncementManager.announcements.forEach(announcement -> {
-                        jsonText.text("§e * ")
-                                .next()
-                                .text(announcement.getTitle())
-                                .next()
-                                .text(" ")
-                                .next()
-                                .text(announcement.getMessage());
+                    announcements.stream()
+                            .filter(Announcement::isActive)
+                            .forEach(announcement -> {
+                                jsonText.text("§e * ")
+                                        .next()
+                                        .text(announcement.getTitle())
+                                        .next()
+                                        .text(" ")
+                                        .next()
+                                        .text(announcement.getMessage());
 
-                        if (announcement.getUrl() != null)
-                            jsonText.clickOpenURL(announcement.getUrl().toExternalForm());
+                                if (announcement.getUrl() != null)
+                                    jsonText.clickOpenURL(announcement.getUrl().toExternalForm());
 
-                        jsonText.next()
-                                .text("\n")
-                                .next();
-                    });
+                                jsonText.next()
+                                        .text("\n")
+                                        .next();
+                            });
 
                     ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
                         User user = UserManager.getUser(proxiedPlayer.getUniqueId());
@@ -75,16 +68,10 @@ public class AnnouncementManager {
         );
     }
 
-    public static List<Announcement> getAnnouncements() {
-        return AnnouncementManager.announcements;
-    }
-
     public static Announcement getAnnouncement(Integer id) {
-        return AnnouncementManager.announcements
-                .stream()
-                .filter(announcement -> announcement.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        AnnouncementDao announcementDao = new AnnouncementDao();
+
+        return announcementDao.findOne("id", id);
     }
 
     public static Announcement toAnnouncement(ResultSet resultSet) throws SQLException, IOException {
