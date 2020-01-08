26e8fa0e9aa008f777ee23fa2791fb7423d21d5b
Gutyerrez <srgutyerrez@gmail.com>
2020-01-08 08:26:10 -0300

    Update RevokeReason.java

--- src/main/java/com/redecommunity/proxy/punish/data/RevokeReason.java
+++ src/main/java/com/redecommunity/proxy/punish/data/RevokeReason.java
@@ -13,4 +13,22 @@ public class RevokeReason {
     private final Integer id;
     private final String name, displayName, description;
     private final Group group;
+
+    public String getDescription() {
+        if (this.description.contains("\\n")) {
+            String[] descriptions = this.description.split("\\\\n");
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < descriptions.length; i++) {
+                String description = descriptions[i];
+
+                stringBuilder.append(description)
+                        .append(i+1 >= descriptions.length ? "" : "\n");
+            }
+
+            return stringBuilder.toString();
+        }
+        return this.description;
+    }
 }
