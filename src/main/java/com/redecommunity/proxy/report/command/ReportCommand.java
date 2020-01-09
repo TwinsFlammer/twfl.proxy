7a1ad40b9490c308dacfad84f7259b25dae8f6cc
Gutyerrez <srgutyerrez@gmail.com>
2020-01-09 08:54:03 -0300

    Update ReportCommand.java

--- src/main/java/com/redecommunity/proxy/report/command/ReportCommand.java
+++ src/main/java/com/redecommunity/proxy/report/command/ReportCommand.java
@@ -2,6 +2,7 @@ package com.redecommunity.proxy.report.command;
 
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
+import com.redecommunity.api.bungeecord.util.JSONText;
 import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.group.manager.GroupManager;
@@ -10,6 +11,8 @@ import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import com.redecommunity.common.shared.report.data.ReportReason;
 import com.redecommunity.common.shared.report.manager.ReportReasonManager;
 import com.redecommunity.proxy.Proxy;
+
+import java.util.List;
 
 /**
  * Created by @SrGutyerrez
@@ -23,7 +26,86 @@ public class ReportCommand extends CustomCommand {
     public void onCommand(User user, String[] args) {
         Language language = user.getLanguage();
 
-        if (args.length != 2) {
+        if (args.length == 1) {
+            String targetName = args[0];
+
+            User user1 = UserManager.getUser(targetName);
+
+            if (user1 == null) {
+                user.sendMessage(
+                        language.getMessage("messages.player.invalid_player")
+                );
+                return;
+            }
+
+            JSONText jsonText = new JSONText();
+
+            List<ReportReason> reportReasons = ReportReasonManager.getReportReasons();
+
+            jsonText.next()
+                    .text("\n")
+                    .next()
+                    .text("§eLista de infração disponíveís (" + reportReasons.size() + ")")
+                    .next()
+                    .text("\n\n");
+
+            reportReasons.forEach(reportReason -> {
+                jsonText.next()
+                        .text(" - ")
+                        .next()
+                        .text(reportReason.getDisplayName())
+                        .hoverText("§e" + reportReason.getDisplayName() +
+                                "\n\n" +
+                                reportReason.getDescription()
+                        )
+                        .clickSuggest("/report " + user1.getDisplayName() + " " + reportReason.getName())
+                        .next()
+                        .text("\n");
+            });
+
+            if (reportReasons.isEmpty()) jsonText.next()
+                    .text("   --/--");
+
+            jsonText.next()
+                    .send(user);
+            return;
+        } else if (args.length == 2) {
+            String targetName = args[0];
+            String reportReasonName = args[1];
+
+            User user1 = UserManager.getUser(targetName);
+
+            if (user1 == null) {
+                user.sendMessage(
+                        language.getMessage("messages.player.invalid_player")
+                );
+                return;
+            }
+
+            ReportReason reportReason = ReportReasonManager.getReportReason(reportReasonName);
+
+            if (reportReason == null) {
+                user.sendMessage(
+                        language.getMessage("report.invalid_reason")
+                );
+                return;
+            }
+
+            user.report(
+                    user1,
+                    reportReason
+            );
+
+            Proxy.broadcastMessage(
+                    GroupManager.getGroup(GroupNames.MODERATOR),
+                    String.format(
+                            language.getMessage("report.report_received"),
+                            user.getPrefix() + user.getDisplayName(),
+                            user1.getPrefix() + user1.getDisplayName()
+                    )
+            );
+            return;
+        } else {
             user.sendMessage(
                     String.format(
                             language.getMessage("messages.default_commands.invalid_usage"),
@@ -33,40 +115,5 @@ public class ReportCommand extends CustomCommand {
             );
             return;
         }
-
-        String targetName = args[0];
-        String reportReasonName = args[1];
-
-        User user1 = UserManager.getUser(targetName);
-
-        if (user1 == null) {
-            user.sendMessage(
-                    language.getMessage("messages.player.invalid_player")
-            );
-            return;
-        }
-
-        ReportReason reportReason = ReportReasonManager.getReportReason(reportReasonName);
-
-        if (reportReason == null) {
-            user.sendMessage(
-                    language.getMessage("report.invalid_reason")
-            );
-            return;
-        }
-
-        user.report(
-                user1,
-                reportReason
-        );
-
-        Proxy.broadcastMessage(
-                GroupManager.getGroup(GroupNames.MODERATOR),
-                String.format(
-                        language.getMessage("report.report_received"),
-                        user.getPrefix() + user.getDisplayName(),
-                        user1.getPrefix() + user1.getDisplayName()
-                )
-        );
     }
 }
