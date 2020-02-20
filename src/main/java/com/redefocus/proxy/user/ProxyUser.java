678280d66bf375f6594d497e97bf6c1807abe1cc
Gutyerrez <srgutyerrez@gmail.com>
2020-02-20 07:33:37 -0300

    Update ProxyUser.java

--- src/main/java/com/redefocus/proxy/user/ProxyUser.java
+++ src/main/java/com/redefocus/proxy/user/ProxyUser.java
@@ -1,39 +1,21 @@
 package com.redefocus.proxy.user;
 
 import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.permissions.user.group.data.UserGroup;
+import com.redefocus.common.shared.preference.Preference;
+import com.redefocus.common.shared.report.data.ReportReason;
+import com.redefocus.common.shared.skin.data.Skin;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by @SrGutyerrez
  */
 public class ProxyUser extends User {
-    public ProxyUser(User user) {
-        super(
-                user.getId(),
-                user.getName(),
-                user.getDisplayName(),
-                user.getUniqueId(),
-                user.getEmail(),
-                user.getPassword(),
-                user.getDiscordId(),
-                user.isTwoFactorAuthenticationEnabled(),
-                user.getTwoFactorAuthenticationCode(),
-                user.getCreatedAt(),
-                user.getFirstLogin(),
-                user.getLastLogin(),
-                user.getLastAddress(),
-                user.getLastLobbyId(),
-                user.getLanguageId(),
-                user.getTwitterAccessToken(),
-                user.getTwitterTokenSecret(),
-                user.getGroups(),
-                user.getPreferences(),
-                user.getFriends(),
-                user.getIgnored(),
-                user.getReports(),
-                user.getSkins(),
-                user.isLogged(),
-                user.isChangingSkin()
-        );
+    public ProxyUser(Integer id, String name, String displayName, UUID uniqueId, String email, String password, Long discordId, Boolean twoFactorAuthenticationEnabled, String twoFactorAuthenticationCode, Long createdAt, Long firstLogin, Long lastLogin, String lastAddress, Integer lastLobbyId, Integer languageId, String twitterAccessToken, String twitterTokenSecret, Collection<UserGroup> groups, List<Preference> preferences, List<Integer> friends, List<Integer> ignored, List<ReportReason> reports, List<Skin> skins, Boolean logged, Boolean changingSkin) {
+        super(id, name, displayName, uniqueId, email, password, discordId, twoFactorAuthenticationEnabled, twoFactorAuthenticationCode, createdAt, firstLogin, lastLogin, lastAddress, lastLobbyId, languageId, twitterAccessToken, twitterTokenSecret, groups, preferences, friends, ignored, reports, skins, logged, changingSkin);
     }
 
     public Boolean isRegistered() {
