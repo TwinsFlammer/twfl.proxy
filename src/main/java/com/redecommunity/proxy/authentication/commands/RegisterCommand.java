fb43da1392f603e65bffcb127f4173f5df6170bf
Gutyerrez <srgutyerrez@gmail.com>
2020-02-29 20:13:35 -0300

    Update RegisterCommand.java

--- src/main/java/com/redecommunity/proxy/authentication/commands/RegisterCommand.java
+++ src/main/java/com/redecommunity/proxy/authentication/commands/RegisterCommand.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
 import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.dao.UserDao;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.util.Helper;
@@ -16,7 +17,7 @@ import java.util.HashMap;
  */
 public class RegisterCommand extends CustomCommand {
     public RegisterCommand() {
-        super("registrar", CommandRestriction.IN_GAME, "default", "register");
+        super("registrar", CommandRestriction.IN_GAME, GroupNames.DEFAULT, "register");
     }
 
     @Override
