307d0630b82cb3961e00f11f178779008da69042
Gutyerrez <srgutyerrez@gmail.com>
2020-02-29 20:17:36 -0300

    Update GroupCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/GroupCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/GroupCommand.java
@@ -3,6 +3,7 @@ package com.redecommunity.proxy.commands.defaults.staff.group;
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
 import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.proxy.commands.defaults.staff.group.arguments.GroupAddCommand;
 import com.redecommunity.proxy.commands.defaults.staff.group.arguments.GroupRemoveCommand;
@@ -14,7 +15,7 @@ public class GroupCommand extends CustomCommand {
     public static final String COMMAND_NAME = "grupo";
 
     public GroupCommand() {
-        super(GroupCommand.COMMAND_NAME, CommandRestriction.ALL, "Manager");
+        super(GroupCommand.COMMAND_NAME, CommandRestriction.ALL, GroupNames.MANAGER);
 
         this.addArgument(
                 new GroupAddCommand(),
