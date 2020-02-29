383ed1776a7cf0aaf696259846badaf4355d4486
Gutyerrez <srgutyerrez@gmail.com>
2020-02-29 20:19:49 -0300

    Update SendCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/SendCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/SendCommand.java
@@ -3,6 +3,7 @@ package com.redecommunity.proxy.commands.defaults.staff;
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
 import com.redecommunity.common.shared.language.enums.Language;
+import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import com.redecommunity.common.shared.server.data.Server;
@@ -13,7 +14,7 @@ import com.redecommunity.common.shared.server.manager.ServerManager;
  */
 public class SendCommand extends CustomCommand {
     public SendCommand() {
-        super("send", CommandRestriction.ALL, "administrator");
+        super("send", CommandRestriction.ALL, GroupNames.ADMINISTRATOR);
     }
 
     @Override
