6022d8c73624aa96776d56833b7e9e952666339c
Gutyerrez <srgutyerrez@gmail.com>
2020-03-13 17:23:02 -0300

    Update GroupCommand.java

--- src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/GroupCommand.java
+++ src/main/java/com/redecommunity/proxy/commands/defaults/staff/group/GroupCommand.java
@@ -51,31 +51,36 @@ public class GroupCommand extends CustomCommand {
 
             JSONText jsonText = new JSONText();
 
-            jsonText.text("\n§eLista de grupos disponíveis (" + groups.size() + "):\n")
+            jsonText.text("\n§eLista de grupos disponíveis (" + groups.size() + "):\n\n")
                     .next();
 
-            groups.forEach(group -> {
-                jsonText.text("  - " + group.getFancyPrefix() + " ")
-                        .next()
-                        .text("§7[Adicionar]")
-                        .clickSuggest("/grupo adicionar " + user1.getDisplayName() + " " + group.getId() + " 0 -1")
-                        .next()
-                        .text(" ")
-                        .next()
-                        .text("§7[Remover]")
-                        .clickSuggest("/grupo remover " + user1.getDisplayName() + " " + group.getId() + " 0")
-                        .next()
-                        .text(" ")
-                        .text("§7[" + server.getDisplayName() + "]")
-                        .clickSuggest("/grupo " + (user1.hasGroup(group, server) ? "adicionar" : "remover") + " " + server.getId() + (user1.hasGroup(group, server) ? "" : " -1"))
-                        .next()
-                        .text("\n")
-                        .next();
-            });
+            groups.stream()
+                    .filter(group -> !group.isDefault())
+                    .sorted((group1, group2) -> group2.getPriority().compareTo(group1.getPriority()))
+                    .forEach(group -> {
+                        jsonText.text("  - " + group.getFancyPrefix() + " ")
+                                .next()
+                                .text("§7[Adicionar]")
+                                .clickSuggest("/grupo adicionar " + user1.getDisplayName() + " " + group.getName().toUpperCase() + " 0 -1")
+                                .next()
+                                .text(" ")
+                                .next()
+                                .text("§7[Remover]")
+                                .clickSuggest("/grupo remover " + user1.getDisplayName() + " " + group.getName().toUpperCase() + " 0")
+                                .next()
+                                .text(" ")
+                                .next()
+                                .text("§7[" + server.getDisplayName() + "]")
+                                .hoverText("§7Clique para " + (user1.hasGroup(group, server) ? "adicionar" : "remover") +
+                                        "\n" +
+                                        "§7Esse grupo ao usuário " + user1.getDisplayName() + ".")
+                                .clickSuggest("/grupo " + (user1.hasGroup(group, server) ? "adicionar" : "remover") + " " + group.getName().toUpperCase() + (user1.hasGroup(group, server) ? "" : " " + server.getId()))
+                                .next()
+                                .text("\n")
+                                .next();
+                    });
 
-            jsonText.text("\n")
-                    .next()
-                    .send(user);
+            jsonText.send(user);
         } else {
             user.sendMessage(
                     language.getMessage("messages.default_commands.groups.help_message")
