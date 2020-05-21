492557faaa53b1e4f14936466dec4ed372cc98a6
Gutyerrez <srgutyerrez@gmail.com>
2020-05-21 04:57:34 -0300

    Update Constants.java

--- src/main/java/br/com/twinsflammer/proxy/cloudflare/util/Constants.java
+++ src/main/java/br/com/twinsflammer/proxy/cloudflare/util/Constants.java
@@ -2,5 +2,5 @@ package br.com.twinsflammer.proxy.cloudflare.util;
 
 public class Constants {
     public static final String BASE_URL = "https://api.cloudflare.com/client/v4/%s";
-    public static final String ZONE_ID = "0595f85c36199df2c765799f5320564b";
+    public static final String ZONE_ID = "db3bb9145b068feba38e63a2679bba62";
 }
